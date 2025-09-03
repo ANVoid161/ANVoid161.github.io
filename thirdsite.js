b45623643101a84be6da0cfae5dbe02fdeca93a7
ANVoid161 <adnguyen2374@gmail.com>
2025-09-03 15:54:30 +0000

    Third Site Update: Javascript Variation

---
+++ thirdsite.js
@@ -0,0 +1,13 @@
+var response = prompt("Password?")
+
+if (response === "I open at the close.") {
+  alert("You shall proceed.")
+}
+
+else if (response === "No.") {
+  alert("Crucio.")
+}
+
+else {
+  alert("Try Again.")
+}
