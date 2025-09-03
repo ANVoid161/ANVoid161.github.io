77afc467488b4301d2929d9e3f9a83a140127259
ANVoid161 <adnguyen2374@gmail.com>
2025-09-03 17:03:31 +0000

    Third Site update: Javascript Variation

--- thirdsite.js
+++ thirdsite.js
@@ -9,6 +9,31 @@ else if (response === "No.") {
   location.href="index.html";
 }
 
+else if (response === "ANVoid161 is not a gamer.") {
+  alert("Am I a joke to you?");
+  location.href="index.html";
+}
+
+else if (response === "I am Mr Gaspard.") {
+  alert("Fine, only because you're my teacher.");
+}
+
+else if (response === "I am Hallebot God.") {
+  alert("AVADA KEDAVRA!");
+  location.href="index.html";
+}
+else if (response === "Again.") {
+  alert("Restart the process.")
+  location.href="secondsite.html"
+}
+else if (response === "The process."){
+  alert("Please stop.")
+location.href="secondsite.html"
+}
+else if (response === "Stop.") {
+  alert("I am getting sick of you.")
+location.href="secondsite.html"
+}
 else {
   alert("Try Again.")
   location.href="secondsite.html"
