f80de6d4d5524ba19fbbddc3da0cf4f8a9c5914a
ANVoid161 <adnguyen2374@gmail.com>
2025-09-04 15:12:36 +0000

    Third Site Update: Javascript Variation

--- thirdsite.js
+++ thirdsite.js
@@ -1,38 +1,38 @@
 var response = prompt("Give me something of importance, then you shall pass.")
 
-if (response === "I open at the close.") {
+if (response === "I open at the close") {
   alert("You shall proceed.")
 }
 
-else if (response === "No.") {
+else if (response === "No") {
   alert("Then get out of my sight.");
   location.href="index.html";
 }
 
-else if (response === "ANVoid161 is not a gamer.") {
+else if (response === "ANVoid161 is not a gamer") {
   alert("Am I a joke to you?");
   location.href="index.html";
 }
 
-else if (response === "I am Mr Gaspard.") {
+else if (response === "I am Mr Gaspard") {
   alert("Fine, only because you're my teacher.");
 }
 
-else if (response === "I am Hallebot God.") {
+else if (response === "I am Hallebot God") {
   alert("You should not be here. BEGONE!");
   location.href="index.html";
 }
-else if (response === "I am Aiden.") {
+else if (response === "I am Aiden") {
   alert("Ah, long time no see!")
 }
-else if (response === "Duster.") {
+else if (response === "Duster") {
   alert("My favorite band. Love listening to their music. Come in.")
 }
 else if (response === "Katy Perry") {
   alert("No, I'm not listening to their music.")
   location.href="secondsite.html"
 }
-else if (response === "Draco dormiens nunquam titillandus.") {
+else if (response === "Draco dormiens nunquam titillandus") {
   alert("Hm, a wise quote. You may enter.")
 }
 else if (response === null) {
