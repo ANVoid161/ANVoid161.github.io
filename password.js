2481ec5e0e92bd3670d7894ebb18d292d4fb018a
ANVoid161 <adnguyen2374@gmail.com>
2025-09-05 18:37:41 +0000

    Button Update

---
+++ password.js
@@ -0,0 +1,76 @@
+var response = prompt("Give me something of importance, then you shall pass.")
+
+if (response === "I open at the close" || response === "Draco Dormiens Nunquam Titillandus") {
+  alert("You shall proceed.")
+  location.href="thirdsite.html"
+}
+
+else if (response === "No") {
+  alert("Then get out of my sight.");
+  location.href="index.html";
+}
+
+else if (response === "ANVoid161 is not a gamer") {
+  alert("Am I a joke to you?");
+  location.href="index.html";
+}
+
+else if (response === "I am Mr Gaspard") {
+  alert("I see. You shall enter then.");
+  location.href="thirdsite.html"
+}
+
+else if (response === "I am Hallebot God") {
+  alert("You should not be here. BEGONE!");
+}
+else if (response === "I am Aiden") {
+  alert("Ah, long time no see!")
+  location.href="thirdsite.html"
+}
+else if (response === "Duster") {
+  alert("My favorite band. Love listening to their music. Come in.")
+  location.href="thirdsite.html"
+}
+else if (response === "Katy Perry") {
+  alert("No, I'm not listening to their music.")
+  location.href="secondsite.html"
+}
+else if (response === ""){
+  alert("At least say something...")
+  location.href="secondsite.html"
+}
+else if (response === "I am Phoebe") {
+  alert("Fine, I guess you can enter.")
+  location.href="thirdsite.html"
+}
+else if (response === "hahayes123") {
+  alert("You datamined this didn't you?")
+}
+else if (response === "I am Nola" || response === "I am Sabrina") {
+  alert("I don't remember you... agh, I guess I can let you in.")
+  location.href="thirdsite.html"
+}
+else if (response === "Cookies") {
+  alert("Ah, Oreo's, you may enter.")
+  location.href="thirdsite.html"
+}
+else if (response === "Goblet of Fire") {
+  alert("'DID YOU PUT YOUR NAME IN THE GOBLET OF FIRE?!?!' ANVoid161 said calmly")
+  location.href="secondsite.html"
+}
+else if (response === "Which came first? The ghost or the poltergeist?") {
+  alert("A non-being can never be. Try something better.")
+  location.href="secondsite.html"
+}
+else if (response === "Architect Celeste") {
+  alert("Ah... you seek more power... I shall give you it if you give a word of importance.")
+  location.href="hahayes.html"
+}
+else if (response === "Upgraded Space Orbit") {
+  alert("I guess there will be a capsule losing contact at some point.")
+  location.href="thirdsite.html"
+}
+else {
+  alert("Try Again.")
+  location.href="secondsite.html"
+}
