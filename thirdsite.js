c3047c1cc99cf25c2db3bd70fadf954c9fefd116
ANVoid161 <adnguyen2374@gmail.com>
2025-09-05 15:25:38 +0000

    The Shadow realm (yes phoebe i took a bit of inspiration srry)

--- thirdsite.js
+++ thirdsite.js
@@ -1,6 +1,6 @@
 var response = prompt("Give me something of importance, then you shall pass.")
 
-if (response === "I open at the close.") {
+if (response === "I open at the close" || response === "Draco Dormiens Nunquam Titillandus") {
   alert("You shall proceed.")
   location.href="thirdsite.html"
 }
@@ -35,16 +35,39 @@ else if (response === "Katy Perry") {
   alert("No, I'm not listening to their music.")
   location.href="secondsite.html"
 }
-else if (response === "Draco dormiens nunquam titillandus") {
-  alert("Hm, a wise quote. You may enter.")
-  location.href="thirdsite.html"
-}
 else if (response === ""){
   alert("At least say something...")
   location.href="secondsite.html"
 }
 else if (response === "I am Phoebe") {
   alert("Fine, I guess you can enter.")
+  location.href="thirdsite.html"
+}
+else if (response === "hahayes123") {
+  alert("You datamined this didn't you?")
+}
+else if (response === "I am Nola" || response === "I am Sabrina") {
+  alert("I don't remember you... agh, I guess I can let you in.")
+  location.href="thirdsite.html"
+}
+else if (response === "Cookies") {
+  alert("Ah, Oreo's, you may enter.")
+  location.href="thirdsite.html"
+}
+else if (response === "Goblet of Fire") {
+  alert("'DID YOU PUT YOUR NAME IN THE GOBLET OF FIRE?!?!' ANVoid161 said calmly")
+  location.href="secondsite.html"
+}
+else if (response === "Which came first? The ghost or the poltergeist?") {
+  alert("A non-being can never be. Try something better.")
+  location.href="secondsite.html"
+}
+else if (response === "Architect Celeste") {
+  alert("Ah... you seek more power... I shall give you it if you give a word of importance.")
+  location.href="hahayes.html"
+}
+else if (response === "Upgraded Space Orbit") {
+  alert("I guess there will be a capsule losing contact at some point.")
   location.href="thirdsite.html"
 }
 else {
