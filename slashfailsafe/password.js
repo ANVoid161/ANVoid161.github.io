80212f89108cfb05140f28b5570679fa58e82765
ANVoid161 <adnguyen2374@gmail.com>
2025-09-10 15:56:44 +0000

    Organizing things

--- slashfailsafe/password.js
+++ slashfailsafe/password.js
@@ -2,22 +2,22 @@ var response = prompt("Give me something of importance, then you shall pass.")
 
 if (response === "I open at the close" || response === "Draco Dormiens Nunquam Titillandus" || response === "Sherbet Lemon") {
   alert("You shall proceed.")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 
 else if (response === "No") {
   alert("Then get out of my sight.");
-  location.href="index.html";
+  location.href="/index.html";
 }
 
 else if (response === "ANVoid161 is not a gamer") {
   alert("Am I a joke to you?");
-  location.href="index.html";
+  location.href="/index.html";
 }
 
 else if (response === "I am Mr Gaspard") {
   alert("I see. You shall enter then.");
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 
 else if (response === "I am Hallebot God") {
@@ -25,71 +25,71 @@ else if (response === "I am Hallebot God") {
 }
 else if (response === "I am Aiden") {
   alert("Ah, long time no see!")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 else if (response === "Duster") {
   alert("My favorite band. Love listening to their music. Come in.")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 else if (response === "Katy Perry") {
   alert("No, I'm not listening to their music.")
-  location.href="secondsite.html"
+  location.href="/secondsite/secondsite.html"
 }
 else if (response === ""){
   alert("At least say something...")
-  location.href="secondsite.html"
+  location.href="/secondsite/secondsite.html"
 }
 else if (response === "I am Phoebe") {
   alert("Fine, I guess you can enter.")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 else if (response === "hahayes123") {
   alert("You datamined this didn't you?")
 }
 else if (response === "I am Nola" || response === "I am Sabrina") {
   alert("I don't remember you... agh, I guess I can let you in.")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 else if (response === "Cookies") {
   alert("Ah, Oreo's, you may enter.")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 else if (response === "Goblet of Fire") {
   alert("'DID YOU PUT YOUR NAME IN THE GOBLET OF FIRE?!?!' ANVoid161 said calmly")
-  location.href="secondsite.html"
+  location.href="/secondsite/secondsite.html"
 }
 else if (response === "Which came first? The ghost or the poltergeist?") {
   alert("A non-being can never be. Try something better.")
-  location.href="secondsite.html"
+  location.href="/secondsite/secondsite.html"
 }
 else if (response === "Architect Celeste") {
   alert("Ah... you seek more power... I shall give you it if you give a word of importance.")
-  location.href="hahayes.html"
+  location.href="/slashfailsafe/hahayes.html"
 }
 else if (response === "Upgraded Space Orbit") {
   alert("I guess there will be a capsule losing contact at some point.")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 else if (response === "Tetris") {
   alert("Hm, my favorite game. You may enter.")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 else if (response === "Something of importance") {
   alert("ha ha. very funny.")
-  location.href="hahayes.html"
+  location.href="/slashfailsafe/hahayes.html"
 }
 else if (response === "Skibidi Toilet") {
   alert("I don't want you here anymore...")
 }
 else if (response === "Contemporary Movement") {
   alert("Me and the Birds.")
-  location.href="thirdsite.html"
+  location.href="/thirdsite/thirdsite.html"
 }
 else if (response === "sfc /scannow") {
   alert("This is not command prompt.")
-  location.href="hahayes.html"
+  location.href="/slashfailsafe/hahayes.html"
 }
 else {
   alert("Try Again.")
-  location.href="secondsite.html"
+  location.href="/secondsite/secondsite.html"
 }
