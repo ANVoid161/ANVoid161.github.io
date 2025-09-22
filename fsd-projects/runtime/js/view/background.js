23aded6d389c2612de2f9a9fa9aa87ac9badc30d
ANVoid161 <adnguyen2374@gmail.com>
2025-09-22 15:49:08 +0000

    tweaking

--- fsd-projects/runtime/js/view/background.js
+++ fsd-projects/runtime/js/view/background.js
@@ -39,18 +39,34 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'cyan');
             background.addChild(backgroundFill);
             
             // TODO 2: - Add a moon and starfield
-            
+            var moon = draw.bitmap('img/moon.png');
+            moon.x = 300;
+            moon.y = 200;
+            moon.scaleX = 10.0;
+            moon.scaleY = 10.0;
+            background.addChild(moon);
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            for (var i = 0; i < 5; ++i) {
+                var buildingHeight = Math.random() * 100;
+                var building = draw.rect(75, buildingHeight, "LightGray", "Black", 1);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building)
+                buildings.push(building);
+            }
             
             // TODO 3: Part 1 - Add a tree
-            
-            
+            var tree;
+            var buildings = [];
+            tree = draw.bitmap("img/tree.png");
+            tree.x = 400;
+            tree.y = 200;
+
         } // end of render function - DO NOT DELETE
         
         
@@ -63,7 +79,10 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+            tree.x = tree.x - 1;
+            if (tree.x < -200) {
+                tree.x = canvasWidth;
+            }
             
             // TODO 4: Part 2 - Parallax
             
